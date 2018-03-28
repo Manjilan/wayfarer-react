19ed4835c659c55c72f472701b1395b20f027469
camario25 <camario25@yahoo.com>
2018-03-28 15:49:31 -0700

    started Signup component

---
+++ src/Signup.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div className="SignupForm">
+      
+      </div>
+    );
+  }
+}
+
+export default Signup;
