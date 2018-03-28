bf0472b5b023c95e1ceb3f5a8072570779dc7639
camario25 <camario25@yahoo.com>
2018-03-28 15:59:01 -0700

    made Signup modal

--- src/Signup.js
+++ src/Signup.js
@@ -4,8 +4,19 @@ import './App.css';
 class Signup extends Component {
   render() {
     return (
-      <div className="SignupForm">
+      <div className="UserForm">
       
+      <form className="Login">
+      <h2>Log in</h2>
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      </form>
+      <form className="Signup">
+      <h2>Signup</h2>
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      <input type="password" placeholder="confirm password"/>
+      </form>
       </div>
     );
   }
