cd5000e5cf4fcce25bd9b8d1d92018c39177870a
camario25 <camario25@yahoo.com>
2018-03-28 16:36:01 -0700

    added Sign up css

--- src/Signup.js
+++ src/Signup.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-import './App.css';
+import './Signup.css';
 
 class Signup extends Component {
   render() {
     return (
       <div className="UserForm">
-      
+      <div className="modalForm">
       <form className="Login">
       <h2>Log in</h2>
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="username"/><br/>
+      <input type="password" placeholder="password"/><br/>
+      <input type="submit" placeholder="login"/>
       </form>
       <form className="Signup">
       <h2>Signup</h2>
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
-      <input type="password" placeholder="confirm password"/>
+      <input type="text" placeholder="username"/><br/>
+      <input type="password" placeholder="password"/><br/>
+      <input type="password" placeholder="confirm password"/><br/>
+      <input type="submit" placeholder="signup"/>
       </form>
+      </div>
       </div>
     );
   }
